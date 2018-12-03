32538e2cd0e59e50d242ac8c2b4a5fc04287ee1f
NataliaPavlova87 <natalia_pavlova@bk.ru>
2018-12-03 23:36:37 +0300

    Update user.js
    Реализация дз по выводу Ф.И.О

--- Pavlova Natalia/src/user.js
+++ Pavlova Natalia/src/user.js
@@ -1,5 +1,14 @@
 import './user.css';
 
-export function sayHello() {
-    alert('Hi');
+export class User {
+    constructor(name, surname, middleName) {
+        this.name = name;
+        this.surname = surname;
+        this.middleName = middleName;
+    }
+
+    sayName() {
+        const text = document.getElementById('name');
+        text.innerHTML = `<p class="text">${this.surname} ${this.name} ${this.middleName}</p>`;
+    }
 }
