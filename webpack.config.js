1afddf37a0d854eaa408c8c15f79567d25c3bdbf
Andrey <voprosi-1@yandex.ru>
2018-12-05 00:52:49 +0300

    00

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 module.exports = {
-    entry:{ main:path.resolve(__dirname, 'src', 'index.js') }
+    entry: {main: path.resolve(__dirname, 'src', 'index.js')},
     output: {
-      path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
     module: {
