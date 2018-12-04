d65a2c504da4f079df8b70c83cd3f53957bf4c44
NataliaPavlova87 <natalia_pavlova@bk.ru>
2018-12-04 17:38:31 +0300

    Update user.js
    Добавление методов для реализации задачи с выводом даты

--- Pavlova Natalia/src/user.js
+++ Pavlova Natalia/src/user.js
@@ -5,10 +5,34 @@ export class User {
         this.name = name;
         this.surname = surname;
         this.middleName = middleName;
+        this.regName =  /^[a-zа-я\-]{1,30}$/i;
+        this.regData = null;
     }
 
     sayName() {
-        const text = document.getElementById('name');
-        text.innerHTML = `<p class="text">${this.surname} ${this.name} ${this.middleName}</p>`;
+        if(this.validParam()) {
+            const text = document.getElementById('name');
+            text.innerHTML = `<p class="text">${this.surname} ${this.name} ${this.middleName}</p>`;
+            return;
+        } 
+    }
+    validParam() {
+        if(this.regName.test(this.name) && this.regName.test(this.surname) && this.regName.test(this.middleName)) {
+            return true;
+        }
+    }
+    lookData() {
+        let date = new Date();
+        let day = date.getDate();
+            if(day < 10) {
+                day = `0${day}`;
+            }
+        let dayWeek = date.getDay();
+            if(dayWeek < 10) {
+                dayWeek = `0${dayWeek}`;
+            }
+        const year = date.getFullYear();
+        const dateObj = `${day}.${dayWeek}.${year}`;
+        console.log(dateObj);
     }
 }
