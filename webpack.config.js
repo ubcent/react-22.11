314d9ac7d7f3b53758c3893c530f59286f9599b6
Andrey <voprosi-1@yandex.ru>
2018-12-05 00:46:36 +0300

    00

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,15 @@ module.exports = {
       path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    }
 };
