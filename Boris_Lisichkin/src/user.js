209791296571b038741d60176a753edee40fa6c8
Lisergide <lisergide@gmail.com>
2018-12-04 23:15:06 +0300

    add code from the lesson 2

---
+++ Boris_Lisichkin/src/user.js
@@ -0,0 +1,11 @@
+import './user.css'
+export default class User {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  sayHi() {
+    return `Hi, ${this.firstName} ${this.lastName}`;
+  }
+}
