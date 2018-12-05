badd42af5f811d5bad3d713bb2e89a0b8151175c
Lisergide <lisergide@gmail.com>
2018-12-05 11:38:45 +0300

    add corrections to file index.js

--- Boris_Lisichkin/src/index.js
+++ Boris_Lisichkin/src/index.js
@@ -1,16 +1,21 @@
 import User from './user';
 import Today from './date';
 
+window.onload = () => {
 
-const user = new User('Vasya', 'Pupkin');
-alert(user.sayHi());
+  const user = new User('Vasya', 'Pupkin');
+  alert(user.sayHi());
 
-const author = new User('Boris', 'Lisichkin');
-author.getAuthor();
+  const author = new User('Boris', 'Lisichkin');
+  author.getAuthor();
 
-const date = new Today();
-const button = document.getElementById('button');
-button.onclick = () => date.addDate();
+  const date = new Today();
+  const button = document.getElementById('button');
+  button.onclick = () => date.addDate();
+
+};
+
+
 
 
 
