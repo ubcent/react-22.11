c73a5ee181e491ecf643532f85268b31c5673403
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-12-05 12:56:41 +0300

    user.js was created with class User and function getFullName

---
+++ MedvedevAlex/src/user.js
@@ -0,0 +1,16 @@
+import './user.css';
+
+export class User {
+  constructor (firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  sayHi() {
+    return `Hi ${this.firstName} ${this.lastName}`
+  }
+}
+
+export function getFullName() {
+  return 'Full Name'
+}
