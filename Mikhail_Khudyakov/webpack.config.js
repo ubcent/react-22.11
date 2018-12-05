e6c55c660a5bdb3544b5615d6039d762def40734
Meeckel <mikhail4189@gmail.com>
2018-12-05 15:56:03 +0300

    Lesson 2

---
+++ Mikhail_Khudyakov/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: { main: path.resolve(__dirname, 'src', 'index.js') },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({ filename: 'style.css' }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        })
+    ]
+};
