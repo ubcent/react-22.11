f3a68fbf916b21d05502624c9ccd32d013a00778
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-12-05 12:55:02 +0300

    webpack.config was updated

---
+++ MedvedevAlex/webpack.config.js
@@ -0,0 +1,23 @@
+// подключение модуля
+const path = require('path');
+
+
+module.exports = {
+  // создание точки входа
+  entry: {main: path.resolve(__dirname, 'src', 'index.js')},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+};
