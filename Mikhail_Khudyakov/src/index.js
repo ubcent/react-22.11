a91e2392b66b3e9fbfbd9438dc6ba7af9c6161a1
Meeckel <mikhail4189@gmail.com>
2018-12-05 16:51:48 +0300

    Lesson 2 completed

--- Mikhail_Khudyakov/src/index.js
+++ Mikhail_Khudyakov/src/index.js
@@ -1,1 +1,11 @@
-alert("React Lesson 2");
+import {Developer} from "./developer";
+import {output} from "./date";
+
+let developer = new Developer('Mikhail', 'Khudyakov', 'Vladimirovich');
+
+$(function () {
+    $('#dev-info').text(developer.sayHi());
+    $('#show-date').on('click', function () {
+        alert(`Текущая дата: ${output}`);
+    });
+});
