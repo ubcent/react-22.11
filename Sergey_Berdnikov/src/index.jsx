7c8e93c3e48dcd30b49468c3d0377e4b833f3f08
Mr.L@mbert_13 <lam_13@mail.ru>
2018-12-07 11:05:47 +0300

    start react lesson

--- Sergey_Berdnikov/src/index.jsx
+++ Sergey_Berdnikov/src/index.jsx
@@ -1,1 +1,20 @@
+//React импортируем в каждом файле
 import React, { Component } from 'react';
+//ReactDom только в точке входа
+import ReactDom from 'react-dom';
+
+class App extends Component
+{
+    render() {
+        return (
+          <div className="box">
+              Hello world
+          </div>
+        );
+    }
+}
+
+ReactDom.render(
+  <App/>,
+  document.getElementById('root'),
+);
