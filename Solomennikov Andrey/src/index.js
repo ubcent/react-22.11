526571c5d1879ceb0c3e1147b4d1a21af9a916e0
Andrey <voprosi-1@yandex.ru>
2018-12-07 09:44:05 +0300

    react-22/11

--- Solomennikov Andrey/src/index.js
+++ Solomennikov Andrey/src/index.js
@@ -1,4 +1,4 @@
-// import User from './user';
+import User from './user';
 //
 // const user = new User('Vasya', 'Pupkin');
 // alert(user.sayHi());
--- Solomennikov Andrey/src/user.js
+++ Solomennikov Andrey/src/user.js
@@ -1,12 +1,12 @@
-// import './user.css';
-//
-// export default class User {
-//     constructor(familyName, firstName) {
-//         this.familyName = familyName;
-//         this.firstName = firstName;
-//     }
-//
-//     sayHi() {
-//         return `Hi, ${this.familyName} ${this.firstName}`;
-//     }
-// }
+import './user.css';
+
+export default class User {
+    constructor(familyName, firstName) {
+        this.familyName = familyName;
+        this.firstName = firstName;
+    }
+
+    sayHi() {
+        return `Hi, ${this.familyName} ${this.firstName}`;
+    }
+}
