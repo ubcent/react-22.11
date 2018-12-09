266704d74b9b5b8860c4de77c691ffe9ddd70d0a
Kurchenkov <43772006+Kurchenkov@users.noreply.github.com>
2018-12-09 21:22:16 +0300

    Add files via upload

---
+++ Kurchenkov/src/user.js
@@ -0,0 +1,10 @@
+export class User{
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    sayHi(){
+        return `Hi, ${this.firstName} ${this.lastName}`;
+    }
+}
