5d4ed04cccff370e4ea1176d28e8f128b0ed7290
NataliaPavlova87 <natalia_pavlova@bk.ru>
2018-12-09 23:30:35 +0300

    Update index.jsx

--- Pavlova Natalia/src/index.jsx
+++ Pavlova Natalia/src/index.jsx
@@ -1,13 +1,18 @@
+import './style.css';
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import Header from './components/Header'
+import Main from './components/Main'
 
 
 class App extends Component {
     render() {
         return (
             <div className="container">
-                <Header />
+                <div className="content">
+                    <Header />
+                    <Main />
+                </div>
             </div>
         );
     }
