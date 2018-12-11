a217ec9377feb1561deb9ba0c5e44dff0f50dbdb
NataliaPavlova87 <natalia_pavlova@bk.ru>
2018-12-11 11:41:04 +0300

    Create Aside.jsx

---
+++ Pavlova Natalia/src/components/Aside/Aside.jsx
@@ -0,0 +1,35 @@
+import './Aside.css';
+
+import React, { Component } from 'react';
+
+import ModalExample from '../ModalExample';
+
+export default class Aside extends Component {
+    render() {
+        return(
+            <aside>
+                <div className="sidebar">
+                    <h2>Search</h2>
+                </div>
+                <div className="sidebar">
+                    <h2>categories</h2>
+                    <nav>
+                        <ul className="categor">
+                            <li><a href="#">Weekend</a></li>
+                            <li><a href="#">Nature</a></li>
+                            <li><a href="#">Work</a></li>
+                            <li><a href="#">Holiday</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="sidebar">
+                    <h2>Stay Tuned.</h2>
+                    <div className="subscribe"></div>
+                </div>
+                <div className="modal">
+                    {/* <ModalExample />  */}
+                </div>
+            </aside>
+        );
+    }
+}
