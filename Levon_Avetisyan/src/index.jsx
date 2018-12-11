b9b67152acaa34a8035bda1fe022fbaf981d86ea
avelevon <levon05@inbox.ru>
2018-12-11 19:43:26 +0300

    files and dependencies

---
+++ Levon_Avetisyan/src/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+import Layout from '/.components/Layout';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Layout />
+            </div>
+        )
+    }
+}
+
+ReactDom.render(
+    <App />,
+    document.getElementById('wrapper'),
+)
