80c97048432b691829bcc64a0218fa6c08c09d1a
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-12-13 15:45:32 +0300

    Main index pages

--- MedvedevAlex/src/index.jsx
+++ MedvedevAlex/src/index.jsx
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
+import Header from './components/header';
+import Content from './components/content';
+
 class App extends Component {
   render() {
-    return (<div>Hello world</div>);
+    return (
+      <div>
+        <Header size="small" color="red"/>
+        <Content/>
+      </div>
+  );
   }
 }
 
-ReactDom.render(<App/>, document.getElementById('root'));
+ReactDom.render(
+  <App/>,
+  document.getElementById('root')
+);
