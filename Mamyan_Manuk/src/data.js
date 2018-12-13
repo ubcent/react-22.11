9cba693c50f0f19c4902c78e34f9afa1a8c3f044
Manuk <mamyanmanuk@yahoo.com>
2018-12-13 12:00:37 +0300

    Lesson 3 HW

--- Mamyan_Manuk/src/data.js
+++
@@ -1,6 +0,0 @@
-export let showData = function () {
-    let data = new Date();
-    let dataDisplay = document.createElement('div');
-    dataDisplay.innerHTML = `${data.getFullYear()}`;
-    document.body.appendChild(dataDisplay);
-}
