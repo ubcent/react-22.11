5bd434253f4822ddf068cd986ccd079e3c66a5e3
NataliaPavlova87 <natalia_pavlova@bk.ru>
2018-12-17 23:38:16 +0300

    Update Main.jsx

--- Pavlova Natalia/src/components/Main/Main.jsx
+++ Pavlova Natalia/src/components/Main/Main.jsx
@@ -1,34 +1,36 @@
 import './Main.css';
 import React, {Component} from 'react';
-
-import Aside from '../Aside';
+import Aside from 'components/Aside';
+import Toggle from 'components/Toggle';
 
 export default class Main extends Component {
     render() {
         return (
-<div className="main-center content">
-        <main>
-            <article>
-
-                <h2>Duis aute irure dolor in henderit in voluptate.</h2>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipop ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...</p>
-                <p><a href="#" className="more">Continue Reading</a></p>
-            </article>
-            <article>
-                <h2>Duis aute irure dolor in henderit in voluptate.</h2>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipop ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...</p>
-                <p><a href="#" className="more">Continue Reading</a></p>
-            </article>
-        </main>
-        <Aside />
-        <div className="clr"> </div>
-        <div className="paper">
-            <p className="pre">&larr;<a href="#">Previous</a></p>
-            <p className="next"><a href="#">Next</a>&rarr;</p>
-        </div>
-    </div>
+            <div className="main-center content">
+                <div className="main">
+                    <main className="main-inside">
+                        <article className="article">
+                            <img src="https://b.radikal.ru/b19/1812/df/e612dba7f42b.jpg" alt="img" />
+                            <h2>Duis aute irure dolor in henderit in voluptate.</h2>
+                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipop ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...</p>
+                            <Toggle />
+                        </article>
+                        <article className="article">
+                            <img src="https://c.radikal.ru/c35/1812/34/e785490bfa3d.jpg" alt="" />
+                            <h2>Duis aute irure dolor in henderit in voluptate.</h2>
+                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipop ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...</p>
+                            <Toggle />
+                        </article>
+                    </main>
+                    <Aside />
+                </div>
+                <div className="paper">
+                    <p className="pre">&larr;<a href="#">Previous</a></p>
+                    <p className="next"><a href="#">Next</a>&rarr;</p>
+                </div>
+            </div>
         );
     }
 }
