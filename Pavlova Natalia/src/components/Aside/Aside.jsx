abf059ba7d54c5263cbd118e519173ba22e24a82
NataliaPavlova87 <natalia_pavlova@bk.ru>
2018-12-17 23:37:44 +0300

    Update Aside.jsx

--- Pavlova Natalia/src/components/Aside/Aside.jsx
+++ Pavlova Natalia/src/components/Aside/Aside.jsx
@@ -1,8 +1,7 @@
 import './Aside.css';
-
 import React, { Component } from 'react';
-
-import ModalExample from '../ModalExample';
+import Search from 'components/Search';
+import FormLogin from 'components/FormLogin';
 
 export default class Aside extends Component {
     render() {
@@ -10,6 +9,7 @@ export default class Aside extends Component {
             <aside>
                 <div className="sidebar">
                     <h2>Search</h2>
+                    <Search />
                 </div>
                 <div className="sidebar">
                     <h2>categories</h2>
@@ -23,12 +23,15 @@ export default class Aside extends Component {
                     </nav>
                 </div>
                 <div className="sidebar">
-                    <h2>Stay Tuned.</h2>
-                    <div className="subscribe"></div>
+                    <h2>Sign In</h2>
+                    <FormLogin />
                 </div>
-                <div className="modal">
-                    <ModalExample /> 
+                <div className="sidebar">
+                    <h2>Stay Tuned</h2>
+                    <div className="subscribe">
+                    </div>
                 </div>
+                <div class="sidebar"> <img src="https://b.radikal.ru/b07/1812/cd/2109a73c8477.jpg" alt="" class="img3" /> </div>
             </aside>
         );
     }
