4d419cfbe275648265e4e22a3f394328484511d1
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-12-20 12:56:29 +0300

    nodeJS cli command cli/components.js --name

--- MedvedevAlex/cli/component.js
+++ MedvedevAlex/cli/component.js
@@ -1,5 +1,44 @@
+const path = require('path');
+const fs = require('fs');
 const minimist = require('minimist');
 
-const args = minimist(process.argv.slice(2));
+const args = minimist(process.argv.slice(2), {
+  alias: {
+    name: 'n',
+  }
+});
 
-console.log(args);
+const componentName = args.name;
+
+fs.mkdirSync(
+  path.resolve(__dirname, '..', 'src', 'components', componentName)
+);
+
+const componentCode = `import './${componentName}.css';
+
+import React, {Component} from 'react';
+import classNames from 'classnames';
+
+export default class ${componentName} extends Component {
+  render() {
+    return (
+      <div className="${componentName}">
+      </div>
+    )
+  }
+}`;
+
+fs.writeFileSync(
+  path.resolve(__dirname, '..', 'src', 'components', componentName, `${componentName}.jsx`),
+  componentCode
+);
+
+fs.writeFileSync(
+  path.resolve(__dirname, '..', 'src', 'components', componentName, `${componentName}.css`),
+  `.${componentName} {}`
+);
+
+fs.writeFileSync(
+  path.resolve(__dirname, '..', 'src', 'components', componentName, 'index.js'),
+  `export default from './${componentName}.jsx';`
+);
