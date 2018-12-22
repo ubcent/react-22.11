3e56914e381236b9947a6882b897f3afd3f2b61e
Kurchenkov <43772006+Kurchenkov@users.noreply.github.com>
2018-12-22 23:18:41 +0300

    Add files via upload

---
+++ Kurchenkov/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css';
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div class="footer">
+                &copy; All rights reserved.
+            </div>
+        );
+    };
+};
