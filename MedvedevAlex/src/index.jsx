081110d7a97ee685a6353233e1c8b7c06ae60f50
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-12-26 12:06:08 +0300

    added normalize css

--- MedvedevAlex/src/index.jsx
+++ MedvedevAlex/src/index.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import 'normalize.css';
 
 import Container from 'components/Container'
 import Header from 'components/Header'
