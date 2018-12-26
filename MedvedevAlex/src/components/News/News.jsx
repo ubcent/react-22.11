24b9fa89facf6a570688bfee24722ac020749d61
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-12-26 12:06:50 +0300

    made some code corrections

--- MedvedevAlex/src/components/News/News.jsx
+++ MedvedevAlex/src/components/News/News.jsx
@@ -13,23 +13,24 @@ getState() {
   const {news} = this.props;
   const object = {};
   for (let i = 0; i < news.length; i++) {
-    object[['text' + (i+1)]] = false;
+    object['text' + (i+1)] = false;
   }
-  console.log(object);
   return object
 }
 
 showTextButton = (event)=> {
   const id = event.target.name;
-  this.state[id] === false ? this.setState({[id] : true,}) : this.setState({[id] : false}) ;
+  this.setState({[id] : !this.state[id]});
 };
 
   render() {
     const {news} = this.props;
     return (
       <div className="News">
-        {news.map((item, idx) => <div key={idx} className="newsBlock"><h3>{item.title}</h3><img src={item.picFilename} alt=""/><p>{item.smallText}</p>
-          <button name={'text' + (idx+1)} onClick={this.showTextButton}>читать далее</button><p>{this.state['text' + (idx+1)] && item.largeText}</p></div>)}
+        {news.map((item, idx) => <div key={idx} className="newsBlock"><h3>{item.title}</h3>
+          <img className="newsBlockImg" src={require(`./${item.picFilename}`)} alt=""/><p>{item.smallText}</p>
+          <button className='showMoreBtn' name={'text' + (idx+1)} onClick={this.showTextButton}>читать далее</button>
+          <p>{this.state['text' + (idx+1)] && item.largeText}</p></div>)}
       </div>
     )
   }
