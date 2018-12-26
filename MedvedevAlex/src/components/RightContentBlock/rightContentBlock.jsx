3e8858404265e40e2848af9c4623f03c2a11dc39
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-12-26 12:07:21 +0300

    changes

--- MedvedevAlex/src/components/RightContentBlock/rightContentBlock.jsx
+++ MedvedevAlex/src/components/RightContentBlock/rightContentBlock.jsx
@@ -2,10 +2,13 @@ import './rightContentBlock.css';
 
 import React, {Component} from 'react';
 
+import Form from 'components/Form';
+
 export default class RightContentBlock extends Component {
   render() {
     return (
       <div className="rightContentBlock">
+        <Form />
       </div>
     )
   }
