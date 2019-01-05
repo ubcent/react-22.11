32ffcb95b3e4577d593a7708ee31a75d440a990d
Kurchenkov <43772006+Kurchenkov@users.noreply.github.com>
2019-01-05 21:59:24 +0300

    Add files via upload

--- Kurchenkov/src/components/Layout/Layout.jsx
+++ Kurchenkov/src/components/Layout/Layout.jsx
@@ -1,12 +1,17 @@
 import './Layout.css';
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import Wrapper from '../Wrapper';
+import Header from '../Header';
+import Content from '../Content';
+import Footer from '../Footer';
 
 export default class Layout extends Component {
     render() {
         return (
-            <Wrapper />
+            <div>
+                <Header />
+                <Content />
+                <Footer />
+            </div>
         );
     };
 };
