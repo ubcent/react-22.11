1517fb5173e5af6fdf574697f8a00c6ddb555a09
Kurchenkov <43772006+Kurchenkov@users.noreply.github.com>
2019-01-06 07:57:20 +0300

    Update Footer.jsx

--- Kurchenkov/src/components/Footer/Footer.jsx
+++ Kurchenkov/src/components/Footer/Footer.jsx
@@ -1,14 +1,13 @@
 import './Footer.css';
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Footer extends Component {
-    render() {
-        return (
-            <div className="footer">
-                <div className="container">
-                    <span className="copyright">Copyright &copy; Your Website 2018</span>
-                </div>
+// stateless
+export default function Footer(props) {
+    return (
+        <div className="footer">
+            <div className="container">
+                <span className="copyright">Copyright &copy; Your Website 2018</span>
             </div>
-        );
-    };
+        </div>
+    );
 };
