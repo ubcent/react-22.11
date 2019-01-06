5c71a1f154014806d9e14fc2c11057afe3aa9e5d
Kurchenkov <43772006+Kurchenkov@users.noreply.github.com>
2019-01-06 09:32:38 +0300

    Update Content.jsx

--- Kurchenkov/src/components/Content/Content.jsx
+++ Kurchenkov/src/components/Content/Content.jsx
@@ -31,34 +31,34 @@ export default class Content extends Component {
         super(props);
 
         this.state = { activeIndex: 0 };
-        this.next = this.next.bind(this);
-        this.previous = this.previous.bind(this);
-        this.goToIndex = this.goToIndex.bind(this);
-        this.onExiting = this.onExiting.bind(this);
-        this.onExited = this.onExited.bind(this);
+        this.next = this.next;
+        this.previous = this.previous;
+        this.goToIndex = this.goToIndex;
+        this.onExiting = this.onExiting;
+        this.onExited = this.onExited;
     }
 
-    onExiting() {
+    onExiting = () => {
         this.animating = true;
     }
 
-    onExited() {
+    onExited = () => {
         this.animating = false;
     }
 
-    next() {
+    next = () => {
         if (this.animating) return;
         const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
         this.setState({ activeIndex: nextIndex });
     }
 
-    previous() {
+    previous = () => {
         if (this.animating) return;
         const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
         this.setState({ activeIndex: nextIndex });
     }
 
-    goToIndex(newIndex) {
+    goToIndex = (newIndex) => {
         if (this.animating) return;
         this.setState({ activeIndex: newIndex });
     }
