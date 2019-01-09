cde64bb099c3cc82961b4a8e060a50ed081e33c2
Victor home <viktor-it@inbox.ru>
2019-01-09 22:16:03 +0400

    1. Настроен роутинг на все страницы блога (созданы страницы блога, но не заполнены контентом) 2. Проведен небольшой рефакторинг кода в части Menu.jsx (удалены ненужные части кода из  reactstrap.io) 3. Частично обновлен контент в блоке News 4. Отредактирована модальная форма авторизации в части CSS

--- viktor-it/src/index.jsx
+++ viktor-it/src/index.jsx
@@ -2,18 +2,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import Layout from 'components/Layout';
+import AboutPage from 'components/AboutPage';
+import NewsPage from 'components/NewsPage';
+import VideoPage from 'components/VideoPage';
+import ContactsPage from 'components/ContactsPage';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 
 export default class App extends Component {
   render() {
    return (
      <div>
-       <Layout />
+       <Switch>{/*не стал выносить это в отдельный файл, пока что мне так удобнее, как завершу сайт, перенесу*/}
+         <Route exact path="/" component={Layout} />
+         <Route exact path="/AboutPage" component={AboutPage} />
+         <Route exact path="/NewsPage" component={NewsPage} />
+         <Route exact path="/VideoPage" component={VideoPage} />
+         <Route exact path="/ContactsPage" component={ContactsPage} />
+       </Switch>
      </div>
    );
   };
 };
 
 ReactDom.render(
-  <App />,
+  <BrowserRouter><App /></BrowserRouter>,
   document.getElementById('root'),
 );
