ded54beae73957900dda3116b7a31740a381a334
Andrey <voprosi-1@yandex.ru>
2019-01-10 16:45:04 +0300

    Заменил в стилях float на inline-block

--- Solomennikov Andrey/src/components/Content/Content.jsx
+++ Solomennikov Andrey/src/components/Content/Content.jsx
@@ -2,27 +2,32 @@ import './Content.css';
 
 import React, { Component } from 'react';
 
-const info = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc libero tortor, ultrices quis vulputate ' +
-    'interdum, feugiat eu lacus. Morbi finibus, ligula non gravida tincidunt, ex est dignissim mauris, vitae ultrices ' +
-    'sem felis vitae velit. Curabitur id quam sollicitudin tellus bibendum porttitor id eget nunc. Aliquam in ' +
-    'iaculis diam. Fusce sapien sapien, sagittis porta urna ut, commodo lobortis sem. Phasellus id magna tincidunt, ' +
-    'maximus odio at, tempus est. Nunc in mauris condimentum, ullamcorper sem vitae, auctor neque. Ut porttitor ' +
-    'rutrum ex in porta. Integer rhoncus tristique sapien sed mollis. Proin sed ex at magna pulvinar consequat ' +
-    'interdum ac ante. Praesent auctor faucibus tincidunt.Mauris lacinia ligula sit amet velit egestas, ac ' +
-    'suscipit dolor interdum. Cras cursus feugiat libero, eu fringilla ipsum facilisis vel. Donec ut ex convallis, ' +
-    'faucibus lorem a, faucibus urna. Nunc diam nisl, pharetra eu condimentum nec, volutpat vel neque. Proin ' +
-    'est tortor, volutpat sit amet lectus at, mollis feugiat sem. In erat libero, tincidunt sed consectetur a, ' +
-    'vehicula at ex. Morbi interdum, lacus a dignissim luctus, mauris velit sagittis magna, eu convallis diam ' +
-    'mauris non est. Donec quis faucibus ligula. Mauris euismod neque quam, non mollis felis tincidunt sit amet. ' +
-    'Phasellus sollicitudin urna sem, eu vehicula erat posuere non. Pellentesque vehicula accumsan diam id suscipit.';
-
 export default class Content extends Component
 {
+    state ={
+        newsOne: 'Gave an OFF THE RECORD luncheon, somewhat of a White House tradition or custom, to network anchors ' +
+            'yesterday - and they quickly leaked the contents of the meeting. Who would believe how bad it has ' +
+            'gotten with the mainstream media, which has gone totally bonkers!',
+        newsTwo: 'The Mainstream Media has NEVER been more dishonest than it is now. NBC and MSNBC are going Crazy. ' +
+            'They report stories, purposely, the exact opposite of the facts. They are truly the Opposition ' +
+            'Party working with the Dems. May even be worse than Fake News CNN, if that is possible!'
+    };
+
+    changeNewsHandler = () => {
+
+         let newNews = this.state.newsTwo;
+
+         this.setState({
+             newsOne: newNews
+        })
+    }
+
     render() {
         return (
                 <div className="article">
                     <img id="logo" src="http://dummyimage.com/700x100/cdd61c" />
-                    <p>{info}</p>
+                    <p>{this.state.newsOne}</p>
+                    <button onClick={this.changeNewsHandler} id="changeNews">Change News</button>
                 </div>
         );
     }
