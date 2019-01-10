cccc837712236f4cdf8f87fd664df0a8cf98cf10
KSFirstUser <rusakk@mail.ru>
2019-01-10 11:39:55 +0300

    delete buttons

--- Sergey_Krikun/src/index.jsx
+++ Sergey_Krikun/src/index.jsx
@@ -39,74 +39,16 @@ class Layout extends Component {
       <div className="layout_main_wrapper">
         <Header />
         <div className="main-content">
-          <div className="buttons-wrapper">
-            <p>To change the content use buttons</p>
-            <br />
-            <Button color="info"
-              onClick={() => this.checkContent('Aticles_list')}
-              className={(this.state.content === 'Aticles_list')
-                ? 'active' : null}>
-              List of articles
-            </Button>
-            <Button color="info"
-              onClick={() => this.checkContent('One_page')}
-              className={(this.state.content === 'One_page')
-                ? 'active' : null}>
-              One article with comments
-            </Button>
-            <Button color="info"
-              onClick={() => this.checkContent('Author_page')}
-              className={(this.state.content === 'Author_page')
-                ? 'active' : null}>
-              Author page
-            </Button>
-            <Button color="info"
-              onClick={() => this.checkContent('List_of_comments')}
-              className={(this.state.content === 'List_of_comments')
-                ? 'active' : null}>
-              List comments
-            </Button>
-            <br />
-          </div>
 
           <Link to="/">Home</Link>
           <Link to="/pageOfArticle/1/2/1">Single Page</Link>
           <div>
             <Switch>
-              {routes.map((route, idx) => {return <Route key={idx} {...route} /> })}
+              {routes.map((route, idx) => { return <Route key={idx} {...route} /> })}
             </Switch>
           </div>
 
-          {/* content == 'Aticles_list' &&
-            <FetchingSelectedData
-              postsStringUrl="/posts?_limit=10"
-              usersStringUrl="/users"
-              mainPage="true"
-            /> */
-          }
 
-          {content == 'One_page' &&
-            <FetchingSelectedData
-              postsStringUrl={`/posts/${numberOfPost}`}
-              usersStringUrl={`/users/${userId}`}
-              commentsStringUrl=
-              {`/comments?postId=${numberOfPost}&_limit=${limitOfComments}`}
-              articleNumber={numberOfPost}
-              pageOfArticle="true"
-            />
-          }
-
-          {content == 'List_of_comments' &&
-            <FetchingSelectedData
-              commentsStringUrl="/comments?_limit=10" commentsPage="true"
-            />
-          }
-
-          {content == 'Author_page' &&
-            <FetchingSelectedData getUsers="true"
-              usersStringUrl="/users?id=1"
-            />
-          }
 
           <hr />
         </div>
